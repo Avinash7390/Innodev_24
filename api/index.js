768f419e98326b84e886150d6fd8e99c6b907c52
IAMAmanRaj <imamanraj87@gmail.com>
2024-03-10 15:24:52 +0530

    backend setup

--- api/index.js
+++ api/index.js
@@ -1,6 +1,47 @@
-import express from 'express';
-const app=express();
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+import path from "path";
+import postRoutes from './routes/post.route.js';
+dotenv.config();
 
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+
+
+
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is listening on port 3000");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use('/api/post',postRoutes);
+
+app.use((err, req, res, next) => {
+  //middleware for handling errors
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
 });
