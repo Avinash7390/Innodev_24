e48721533c174fc21659dac51843195aa7a75308
V Suhas <suhas3145@gmail.com>
2024-03-13 15:39:25 +0530

    added image feature

--- client/src/pages/GlobalChat.jsx
+++ client/src/pages/GlobalChat.jsx
@@ -22,9 +22,35 @@ const GlobalChat = () => {
         <div>
             <main className="content">
                 <div className="container mt-3">
-                    {user && <div>Logged in as  {user}</div>}
-                    {!user &&
-                    <NewChatUser newUser={newUser} handleChange={handleChange} logNewUser={logNewUser} />
+                    {user && (
+                        <div className="card w-100">
+                            <div className="row">
+                                <div className="col-12 col-lg-12 col-xl-12">
+                                <div className="py-2 px-4 w-100 border-bottom d-lg-block sticky-top bg-white">
+                                    <div className="d-flex align-items-center py-1">
+                                        <div className="position-relative">
+                                            <img 
+                                            src="" 
+                                            className="rounded-circle mx-2"
+                                            alt={user}
+                                            width="40"
+                                            height="40"
+                                            />
+                                        </div>
+
+                                        <div className="flex-grow-1">
+                                            <strong>Logged in as {user}</strong>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                    }
+                    {!user &&(
+                    <NewChatUser newUser={newUser} handleChange={handleChange} logNewUser={logNewUser} />)
                     }
 
                 </div>
