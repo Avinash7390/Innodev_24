1a5058d5dae490d792c382165f4de52cedcf7dcc
V Suhas <suhas3145@gmail.com>
2024-03-14 14:10:20 +0530

    chat feature

--- client/src/pages/GlobalChat.jsx
+++ client/src/pages/GlobalChat.jsx
@@ -3,11 +3,17 @@ import "font-awesome/css/font-awesome.css";
 import { io } from "socket.io-client";
 import React, { useState } from 'react'
 import NewChatUser from "../components/NewChatUser";
+import Chat from "../components/chat";
+
+
 
 const GlobalChat = () => {
 
     const [newUser, setNewUser] = useState("");
     const [user, setUser] = useState("");
+    const [Message,setMessage] = useState("");
+
+
     function handleChange({ currentTarget: input }) {
         setNewUser(input.value);
     }
@@ -16,39 +22,17 @@ const GlobalChat = () => {
         setUser(newUser);
     }
 
+    function handleChange2({ currentTarget: input }){
+        setMessage(input.value)
+    }
+
 
 
     return (
         <div>
             <main className="content">
                 <div className="container mt-3">
-                    {user && (
-                        <div className="card w-100">
-                            <div className="row">
-                                <div className="col-12 col-lg-12 col-xl-12">
-                                <div className="py-2 px-4 w-100 border-bottom d-lg-block sticky-top bg-white">
-                                    <div className="d-flex align-items-center py-1">
-                                        <div className="position-relative">
-                                            <img 
-                                            src="" 
-                                            className="rounded-circle mx-2"
-                                            alt={user}
-                                            width="40"
-                                            height="40"
-                                            />
-                                        </div>
-
-                                        <div className="flex-grow-1">
-                                            <strong>Logged in as {user}</strong>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                </div>
-                            </div>
-                        </div>
-                    )
-                    }
+                    {user && (<Chat user={user} Message={Message} handleChange2={handleChange2}/>)}
                     {!user &&(
                     <NewChatUser newUser={newUser} handleChange={handleChange} logNewUser={logNewUser} />)
                     }
